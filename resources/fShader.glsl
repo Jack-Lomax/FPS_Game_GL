63945b1d08d43c2064355b830ff65e5c14035648
Barji <66711017+BarjiYT@users.noreply.github.com>
2022-06-27 17:47:14 +0100

    Day 3 - Textures

--- resources/fShader.glsl
+++ resources/fShader.glsl
@@ -1,7 +1,13 @@
 #version 330 core
 out vec4 FragColor;
 in vec3 vertexColor;
+in vec2 texCoord;
+
+uniform sampler2D ourTexture;
+
+uniform float time;
+
 void main()
 {
-   FragColor = vec4(vertexColor, 1.0);
+   FragColor = texture(ourTexture, texCoord) * abs(sin(time));// * vec4(vertexColor, 1.0);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "images/stb_image.h"
 #include <glad/glad.h>
 #define GLFW_DLL
 #include <glfw/glfw3.h>
@@ -16,11 +18,11 @@ int main()
 {
 	float vertices[] = 
 	{
-		//Positions			//Colors
-		0.5f, 0.5f, 0.0f, 	1.0f, 0.0f, 0.0f,
-		0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
-		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-		-0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+		//Positions			//Colors			//Tex Coords
+		0.5f, 0.5f, 0.0f, 	1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+		0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,	0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
 	};
 
 	unsigned int indices[] = 
@@ -71,16 +73,43 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 6, (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 8, (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 6, (void*)(3 * sizeof(GL_FLOAT)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 8, (void*)(3 * sizeof(GL_FLOAT)));
 	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 8, (void*)(6 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
 	#pragma endregion
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	unsigned int texture;
+	glGenTextures(1, &texture);	
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);  
+	unsigned char* data = stbi_load("../resources/saul.png", &width, &height, &nrChannels, 0);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(data);
+
+
 
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	while (!glfwWindowShouldClose(window))
@@ -89,6 +118,9 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		float time = glfwGetTime();
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		shader.SetFloat("time", glfwGetTime());
 		shader.Use();
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
